1dcc4541c8bebd5c80e77453395a6bfddcdf5ff1
Orz3br4 <97266424+Orz3br4@users.noreply.github.com>
2023-03-27 14:10:38 +0800

    Implement composition line square part

--- src/main/java/component/UMLComposition.java
+++ src/main/java/component/UMLComposition.java
@@ -11,5 +11,35 @@ public class UMLComposition extends UMLBasicLine {
     public void draw(Graphics g) {
         g.setColor(Color.BLACK);
         g.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+
+        Point v = new Point(startPoint.x - endPoint.x, startPoint.y- endPoint.y );
+        int vLength = (int) Math.hypot(v.x, v.y);
+
+        Point p1 = new Point(
+                (int) ((v.x * Math.cos(Math.PI / 4) - v.y * Math.sin(Math.PI / 4)) / vLength * 12 + endPoint.x),
+                (int) ((v.x * Math.sin(Math.PI / 4) + v.y * Math.cos(Math.PI / 4)) / vLength * 12 + endPoint.y)
+        );
+
+        Point p2 = new Point(
+                (int) ((v.x * Math.cos(-Math.PI / 4) - v.y * Math.sin(-Math.PI / 4)) / vLength * 12 + p1.x),
+                (int) ((v.x * Math.sin(-Math.PI / 4) + v.y * Math.cos(-Math.PI / 4)) / vLength * 12 + p1.y)
+        );
+
+        Point p3 = new Point(
+                (int) ((v.x * Math.cos(-Math.PI / 4) - v.y * Math.sin(-Math.PI / 4)) / vLength * 12 + endPoint.x),
+                (int) ((v.x * Math.sin(-Math.PI / 4) + v.y * Math.cos(-Math.PI / 4)) / vLength * 12 + endPoint.y)
+        );
+
+        g.setColor(Color.WHITE);
+        g.fillPolygon(
+                new int[] {p1.x, p2.x, p3.x, endPoint.x},
+                new int[] {p1.y, p2.y, p3.y, endPoint.y},
+                4);
+
+        g.setColor(Color.BLACK);
+        g.drawPolygon(
+                new int[] {p1.x, p2.x, p3.x, endPoint.x},
+                new int[] {p1.y, p2.y, p3.y, endPoint.y},
+                4);
     }
 }
