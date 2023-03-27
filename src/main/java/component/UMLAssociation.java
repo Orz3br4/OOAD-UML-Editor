0509d495f1ba0f6f8160c9e9a3c26c82643b0cb0
Orz3br4 <97266424+Orz3br4@users.noreply.github.com>
2023-03-27 12:32:57 +0800

    draw arrow part in UMLAssociation

--- src/main/java/component/UMLAssociation.java
+++ src/main/java/component/UMLAssociation.java
@@ -12,7 +12,22 @@ public class UMLAssociation extends UMLBasicLine {
         g.setColor(Color.BLACK);
         g.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
 
-        
-//        g.
+        Point v = new Point(startPoint.x - endPoint.x, startPoint.y- endPoint.y );
+        int vLength = (int) Math.hypot(v.x, v.y);
+
+        Point arrowEndPoint1 = endPoint;
+        Point arrowStartPoint1 = new Point(
+                (int) ((v.x * Math.cos(Math.PI / 6) - v.y * Math.sin(Math.PI / 6)) / vLength * 10 + endPoint.x),
+                (int) ((v.x * Math.sin(Math.PI / 6) + v.y * Math.cos(Math.PI / 6)) / vLength * 10 + endPoint.y)
+        );
+
+        Point arrowEndPoint2 = endPoint;
+        Point arrowStartPoint2 = new Point(
+                (int) ((v.x * Math.cos(-Math.PI / 6) - v.y * Math.sin(-Math.PI / 6)) / vLength * 10 + endPoint.x),
+                (int) ((v.x * Math.sin(-Math.PI / 6) + v.y * Math.cos(-Math.PI / 6)) / vLength * 10 + endPoint.y)
+        );
+
+        g.drawLine(arrowStartPoint1.x, arrowStartPoint1.y, arrowEndPoint1.x, arrowEndPoint1.y);
+        g.drawLine(arrowStartPoint2.x, arrowStartPoint2.y, arrowEndPoint2.x, arrowEndPoint2.y);
     }
 }
